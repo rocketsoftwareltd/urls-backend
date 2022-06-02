22a8092cd5a43a8c9feedd870a4d17a75bdf5e4c
Faith Philemon <philemonfaith@gmail.com>
2022-06-02 03:10:39 -0700

    clean ups

--- index.php
+++ index.php
@@ -8,11 +8,21 @@
 //     'gender'   => "male"
 // ];
 
-$new_array = [
-    "username = Faith",
-    "password = @#$%^",
-    "gender   = male"
-];
+// $array = [2, 4, 6, 8, 0];
+
+// $array = "gender => male";
+
+// $new_array = array_push($array, "Pilemon");
+
+// print_r($array);
+
+// echo "<br><br><br>";
+
+// $new_array = [
+//     "username = Faith",
+//     "password = @#$%^",
+//     "gender   = male"
+// ];
 
 // print_r($array);
 
@@ -23,4 +33,36 @@ $new_array = [
 // echo $imploded;
 
 
+// $alphanumeric = md5("faith");
 
+
+// echo "The hashed version of <strong>faith</strong> is <br>". $alphanumeric;
+
+// $alphanumeric  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// $char_array = str_split($alphanumeric);
+// $random_string = "";
+// $lower_limit = rand(1, 57);
+// $count = 0;
+
+// for($i = $lower_limit; $i <= strlen($alphanumeric); $i++){
+//     $rand = rand(0, 61);
+//     $random_string .= $char_array[$rand];
+//     $count++;
+//     if($count == 5){
+//         break;
+//     }
+// }
+
+
+// $char_array = str_split(str_shuffle($alphanumeric));
+// for($i = 0; $i < 5; $i++){
+//     $random_string .= $char_array[$i];
+// }
+
+
+// echo "<br>". $random_string;
+
+
+// $hashed_password = password_hash("1234", PASSWORD_DEFAULT);
+
+// echo $hashed_password;
